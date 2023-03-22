c47e545655a2f37a29b81554fe8afe3326878092
google <google@gmail.com>
2023-03-22 18:48:09 +0800

    fix dep

--- spring-debug/src/main/java/com/chenyi/springdebug/SpringDebugApplication.java
+++ spring-debug/src/main/java/com/chenyi/springdebug/SpringDebugApplication.java
@@ -1,13 +1,18 @@
 package com.chenyi.springdebug;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 
 //@SpringBootApplication
 public class SpringDebugApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(SpringDebugApplication.class, args);
+//		 SpringApplication.run(SpringDebugApplication.class, args);
+
+		System.out.println(DateUtil.now());
 
 	}
 
