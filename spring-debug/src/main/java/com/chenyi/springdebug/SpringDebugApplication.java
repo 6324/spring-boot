e07ddefda9bfd3be7372ac36957c9d5e517adf91
he.zhao <Zhaohe111>
2023-03-22 23:20:15 +0800

    fix

--- spring-debug/src/main/java/com/chenyi/springdebug/SpringDebugApplication.java
+++ spring-debug/src/main/java/com/chenyi/springdebug/SpringDebugApplication.java
@@ -1,19 +1,22 @@
 package com.chenyi.springdebug;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import cn.hutool.system.SystemUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.StrUtil;
+//import cn.hutool.core.util.StrUtil;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class SpringDebugApplication {
 
 	public static void main(String[] args) {
-//		 SpringApplication.run(SpringDebugApplication.class, args);
-
 		System.out.println(DateUtil.now());
+		System.out.println(SystemUtil.getJavaInfo());
+		System.out.println(SystemUtil.getJavaRuntimeInfo());
+		System.out.println(SystemUtil.getJavaSpecInfo());
 
+		 SpringApplication.run(SpringDebugApplication.class, args);
 	}
 
 }
